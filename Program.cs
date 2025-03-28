420f5402b92517f311800d990145c2ba26222304
amoy <ali.moaydi2020@gmail.com>
2025-03-28 14:10:40 +0330

    fix: listen on 0.0.0.0 for docker compatibility

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers();
